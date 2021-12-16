aa5460477957b0780dc75d8d2bb6bfac14390cba
tdelort <tristan.delort@gmail.com>
2021-12-16 13:29:21 -0500

    Fix newspaper text and credits button that was ugly

--- Assets/NewsBehaviour.cs
+++ Assets/NewsBehaviour.cs
@@ -1,35 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewsBehaviour : MonoBehaviour
 {
     public GameObject headLine;
-    public GameObject text1, text2, text3;
+    public Text text1, text2, text3;
     // Start is called before the first frame update
+    public int uichoice = 1;
     void Start()
     {
         int choice = GameManager.currentLevel;
+        choice = uichoice;
         if (choice == 1)
         {
             headLine.GetComponent<TextMesh>().text = "ELECTIONS PRESIDENTIELLES";
-            text1.GetComponent<TextMesh>().text = "Le parti du travail accède \nau pouvoir avec 60% des \nvoix. Nous continuerons\nde vous informer des\n premieres mesures de ce \nnouveau parti et espérons \nprospérité pour notre beau \npays. rendez vous \nen page 2 pour en savoir \nplus sur le parti";
-            text2.GetComponent<TextMesh>().text = "Victoire de l'équipe nationale d'eudéo dans \nson match de préparation au mondial de \nfootball. Retour avec les commentaires du \ncoach sur le match en page 3.";
-            text3.GetComponent<TextMesh>().text = "Il y a maintenant de nouvelles règles de \ndéplacements, tous citoyens peut se \ndéplacer librement à condition qu'il ait \navec soit un passeport valide. Il y a également\nde nouveaux controlleurs dans les rames \nde métro ou dans les centres commercials";
+            text1.text = "Le parti du travail accède au pouvoir avec 60% des voix. Nous continuerons de vous informer des premieres mesures de ce nouveau parti et espérons prospérité pour notre beau pays. rendez vous en page 2 pour en savoir plus sur le parti";
+            text2.text = "Victoire de l'équipe nationale d'eudéo dans son match de préparation au mondial de football. Retour avec les commentaires du coach sur le match en page 3.";
+            text3.text = "Les sanctions pour titre de transport manquant sont revues à la hausse. Préparez vous a des amende allant jusqu'a 10 000£ et 6 mois emprisonnement en cas de récidive.";
         }
         if (choice == 2)
         {
             headLine.GetComponent<TextMesh>().text = "RENATIONALISATION DU PAYS";
-            text1.GetComponent<TextMesh>().text = "Une des premieres mesures \navec celles d'hier du nouveau \nparti a été de lancer une \ncampagne d'expulsion des \nsans-papiers de notre pays. Une \naction a déjà été lancé dans le \nquartier est et une petite \ncentaine de personne y ont déjà \nété expulsé.";
-            text2.GetComponent<TextMesh>().text = "Les citoyens de l'Eudéo ont maintenant besoin \nd'une autorisation de sortie afin de pouvoir \ncontroler d'une meilleur façon les sans papiers \net de pouvoir mener à bien l'opération.";
-            text3.GetComponent<TextMesh>().text = "Les matchs de Football sont suspendus jusqu'à \nnouvel ordre. Retour avec l'ensemble de \nl'équipe sur l'année parcouru en page 2 \net organisation de notre propre compétition \nau sein de notre pays.";
+            text1.text = "Une des premieres mesures avec celles d'hier du nouveau parti a été de lancer une campagne d'expulsion des sans-papiers de notre pays. Une action a déjà été lancé dans le quartier est et une petite centaine de personne y ont déjà été expulsé.";
+            text2.text = "Les matchs de Football sont suspendus jusqu'à nouvel ordre. Retour avec l'ensemble de l'équipe sur l'année parcouru en page 2 et organisation de notre propre compétition au sein de notre pays.";
+            text3.text = "Il y a maintenant de nouvelles règles de déplacements, tous citoyens peut se déplacer librement à condition qu'il ait avec soit un passeport ou une pièce d'identité valide. Il y a également de nouveaux controlleurs dans les rames de métro ou dans les centres commerciaux";
         }
         if (choice == 3)
         {
             headLine.GetComponent<TextMesh>().text = "L' ANCIEN PARTI S'ORGANISE";
-            text1.GetComponent<TextMesh>().text = "Il a été découvert que l'ancien \nparti s'organise en groupe afin \nde mener des actions \nterroristes contre le parti en \nplace. Il est demandé de \nreporter à la police toutes \npersonnes ou activités \nsuspectes afin de prévenir \ntous problèmes.";
-            text2.GetComponent<TextMesh>().text = "Les règles dictées hier ainsi qu'avant-hier risquent de rester jusqu'à nouvel ordre et un renforcement des contrôles sera effectués dans les trains. Il y a interdiction d'entrerdans le pays jusqu'à nouvel ordre";
-            text3.GetComponent<TextMesh>().text = "Les actions d'hier et d'avant-hier a pu \npermettre de nettoyer d'autres quartiers \net d'arretter d'autres personnes illégales. ";
+            text1.text = "Il a été découvert que l'ancien parti s'organise en groupe afin de mener des actions terroristes contre le parti en place. Il est demandé de reporter à la police toutes personnes ou activités suspectes afin de prévenir tous problèmes.";
+            text2.text = "Les règles dans les transports risquent de rester jusqu'à nouvel ordre et un renforcement des contrôles sera effectués dans les trains. Il y a interdiction d'entrer dans le pays jusqu'à nouvel ordre";
+            text3.text = "La loi sur l'immigration a permit de nettoyer d'autres quartiers et d'arretter d'autres personnes illégales. Le porte parole du gouvernement prendra l'antenne à 19h ce soir pour réagir sur cette victoire.";
         }
         
     }
